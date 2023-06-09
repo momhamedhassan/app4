8dd3cd094948493853e14084e1dafe033cce02c6
mohamed hassan <120303364+momhamedhassan@users.noreply.github.com>
2023-06-09 22:17:39 +0300

    new

--- controllers/Article/ArticleCommunication.js
+++ controllers/Article/ArticleCommunication.js
@@ -3,40 +3,72 @@ const { default: mongoose } = require('mongoose');
 const DoctorModel=require('../../Models/Doctor.Model')
 const Article = require('../../Models/Article/Article');
 const ArticleCommunocation=require('../../Models/Article/ArticleCommunocation')
+const AgoraUser=require('./../../Models/AgoraUser')
 module.exports=
 {
     patchArticleLikes:async(req,res,next)=>{
         const id = req.params.id;
-        const updates=req.body;
+        const userId=req.payload.aud.replace(/['"]+/g, '');
+        var data=[{"Doctor":userId}];
         const options={new :true}
               try 
               {
-                const addComment =await ArticleCommunocation.findOneAndUpdate(
-                {_id:id}, 
-                { $addToSet: {Likes:updates.Likes},},
-                )
-                const result=await ArticleCommunocation.findById({_id:id});
-                if(!result){throw createError(404,"Product does not exist ")}
-                res.send(result)
+                const check=await ArticleCommunocation.findOne({'Likes.Doctor':userId})
+                if(!check){
+                    console.log('like')
+                    const addLikes =await ArticleCommunocation.findOneAndUpdate(
+                        {_id:id}, 
+                        { $addToSet: {Likes:data},},
+                        )
+                        const result=await ArticleCommunocation.findById({_id:id});
+                        if(!result){throw createError(404,"ArticleCommunication does not exist ")}
+                        res.send(result)
+                }else{
+                    console.log('dislike')
+                    const addComment =await ArticleCommunocation.findOneAndUpdate(
+                        {_id:id}, 
+                        { $pull: {Likes:{Doctor:userId}},},
+                        );
+                        const result=await ArticleCommunocation.findById({_id:id});
+                        if(!result){throw createError(404,"ArticleCommunication does not exist ")}
+                        res.send(result)
+                }
+               
+        
               } 
                  
               catch (error) 
               {
                 console.log(error.message)
                 if (error instanceof mongoose.CastError)
-                {return next(createError(400,"Invalid Product Id"))}
+                {return next(createError(400,"Invalid ArticleCommunication Id"))}
                 next(error)
               }  
 
     },
-    getAllArticleCommunicationById:async(req,res,next)=>{
+    getArticleCommunicationById:async(req,res,next)=>{
         const id =req.params.id
         try{
-            const results=await ArticleCommunocation.findById({_id:id},{})
+            console.log('... hello from article commuinacation ...')
+            const results=await ArticleCommunocation.findById({_id:id})
             .populate(
             {path:'Comments.Doctor',
-            model:DoctorModel,
-            select:{__v:0}
+            model:AgoraUser,
+            select:{__v:0,access_token:0,created_at:0,expire_date:0,token:0,password:0,userType:0,type:0},
+            populate:{
+                path:'doctorId',
+                model:DoctorModel,
+                select:{__v:0}
+            }
+            }).populate(
+            {path:'Likes.Doctor',model:AgoraUser,
+            select:{__v:0,access_token:0,created_at:0,expire_date:0,token:0,password:0,userType:0,type:0},
+            populate:{
+                path:'doctorId',
+                model:DoctorModel,
+                select:{__v:0}
+            }
+            
             }).exec();
             res.send(results)
         }catch(error){
@@ -56,9 +88,7 @@ module.exports=
     ,
     postArticleCommunication:async(req,res,next)=>{
         try{
-            console.log(typeof(req.body))
-            var item={}
-            console.log(typeof(item))
+           
             const article= await ArticleCommunocation.create(req.body);
             console.log(article)
             res.send(article)
@@ -78,12 +108,12 @@ module.exports=
         try {
             const result =await ArticleCommunocation.find({_id:id},{Comments:0,__v:0})
             .populate({
-                path:"Likes",
-                model:DoctorModel,
-                select:{__v:0}
+                path:"Likes.Doctor",
+                model:AgoraUser,
+                select:{__v:0,access_token:0,created_at:0,expire_date:0,token:0,password:0,userType:0,type:0}
             })
             if(!result){
-                throw createError(404,"Product does not exist")
+                throw createError(404,"ArticleCommunication does not exist")
                 
                    }
             console.log(result)
@@ -92,7 +122,7 @@ module.exports=
             console.log(error.message)
             if(error instanceof mongoose.CastError){
         
-                next(createError(400,"Invalid product id"))
+                next(createError(400,"Invalid ArticleCommunication id"))
                 return;
             }
             next(error);
@@ -101,18 +131,27 @@ module.exports=
     },
     AddComment:async(req,res,next)=>{
         const id = req.params.id;
+        const userId=req.payload.aud.replace(/['"]+/g, '');
+        
         const updates=req.body;
+       
+        updates.Comments.forEach(element => {
+        element.Doctor=userId;
+        console.log(element)
+        });
+        
+        console.log(updates)
         const options={new :true}
-            // res.send("updating a single product")
+         
               try 
               {
-                // console.log(updates.Comments)
+                
                 const addComment =await ArticleCommunocation.findOneAndUpdate(
                 {_id:id}, 
                 { $addToSet: {Comments:updates.Comments},},
                 );
                 const result=await ArticleCommunocation.findById({_id:id});
-                if(!result){throw createError(404,"Product does not exist ")}
+                if(!result){throw createError(404,"ArticleCommunication does not exist ")}
                 res.send(result)
               } 
                  
@@ -120,7 +159,7 @@ module.exports=
               {
                 console.log(error.message)
                 if (error instanceof mongoose.CastError)
-                {return next(createError(400,"Invalid Product Id"))}
+                {return next(createError(400,"Invalid ArticleCommunication Id"))}
                 next(error)
               }  
     },deleteComment:async(req,res,next)=>{
@@ -136,7 +175,7 @@ module.exports=
                 { $pull: {Comments:{_id:commentId}},},
                 );
                 const result=await ArticleCommunocation.findById({_id:articleCommunicationId});
-                if(!result){throw createError(404,"Product does not exist ")}
+                if(!result){throw createError(404,"ArticleCommunication does not exist ")}
                 res.send(result)
               } 
                  
@@ -144,7 +183,7 @@ module.exports=
               {
                 console.log(error.message)
                 if (error instanceof mongoose.CastError)
-                {return next(createError(400,"Invalid Product Id"))}
+                {return next(createError(400,"Invalid ArticleCommunication Id"))}
                 next(error)
               }  
     }
@@ -162,7 +201,7 @@ module.exports=
                 { $pull: {Likes:{_id:commentId}},},
                 );
                 const result=await ArticleCommunocation.findById({_id:articleCommunicationId});
-                if(!result){throw createError(404,"Product does not exist ")}
+                if(!result){throw createError(404,"ArticleCommunication does not exist ")}
                 res.send(result)
               } 
                  
@@ -170,7 +209,7 @@ module.exports=
               {
                 console.log(error.message)
                 if (error instanceof mongoose.CastError)
-                {return next(createError(400,"Invalid Product Id"))}
+                {return next(createError(400,"Invalid ArticleCommunication Id"))}
                 next(error)
               }  
     }
---
+++ route/Auth.route.js
@@ -0,0 +1,224 @@
+const bcrypt=require('bcrypt')
+const express =require('express')
+const router=express.Router()
+const createError=require('http-errors')
+const nodemailer=require("nodemailer");
+const AgoraUser=require('../Models/AgoraUser')
+const Patient =require("./../Models/Patient.Model")
+const {authSchema,AgoraUserAuthSchema}=require('../Database/validation_schema')
+const {signAccessToken,agoraSignAccessToken}=require('../Database/jwt_helper');
+const Doctor = require('../Models/Doctor.Model');
+
+let transporter =nodemailer.createTransport({
+  service:"gmail",
+  auth:{
+      user:process.env.AUTH_EMAIL,
+      pass:process.env.AUTH_PASS
+
+  }
+});
+
+transporter.verify((error,success)=>{
+  if(error){
+      console.log(error);
+  }else{
+      console.log("Ready for messages");
+      console.log(success)
+  }
+})
+router.get('/',async(req,res,next)=>{
+const users=await AgoraUser.find({});
+res.json(users)
+});
+router.post('/register',async(req,res,next)=>{
+    try {
+        
+        const result=await authSchema.validateAsync(req.body)
+        const doesExist =await User.findOne({email:result.email}).exec();
+
+        if(doesExist){
+            throw createError.Conflict(`${result.email} is already been regitered `)
+        }
+
+        const user=await User.create(result)
+        const accessToken=await signAccessToken(user.id)
+        res.send({accessToken})
+
+    } catch (error) {
+        if(error.isJoi===true)error.status=422
+        next(error)
+    }
+})
+router.post('/login',async(req,res,next)=>{
+  const token_expire_date='1h';
+  try {
+    const result=await authSchema.validateAsync(req.body)
+    
+    const user=await User.findOne({email:result.email})
+    if(!user)throw createError.NotFound("user not register")
+    
+    const isMatch=await user.isValidPassword(result.password)
+    if(!isMatch)throw createError.Unauthorized('Username/password not valid')
+
+    const accessToken=await signAccessToken(user.id,token_expire_date)
+   
+    res.send({accessToken})
+  } catch (error) {
+    if(error.isJoi===true)return next(createError.BadRequest("Invalid Username/Password"))
+    next(error)
+  }
+})
+router.post('/Agoralogin',async(req,res,next)=>{
+   const email =req.body.email;
+    console.log(email)
+    const password=req.body.password;
+    console.log(password)
+    const type  =req.body.type;
+    console.log(type)
+    const userType=req.body.userType;
+    console.log(userType)
+    
+
+  const mailOptions={
+    from:process.env.AUTH_EMAIL,
+    to: email,
+    subject:"Verify Your Email",
+    html:`<p> Welcome to care link </p>`
+};
+
+  const token_expire_date='365d';
+  const access_token_expire_date='12h'
+  console.log("hello from Agora login ")
+  console.log("... result data ..."+req.body)
+  try {
+    const result=await AgoraUserAuthSchema.validateAsync(req.body)
+    var user;
+    let map = {};
+    map['type'] = result['type'];
+    map['open_id'] = result['open_id'];
+   
+    if(type==2){
+       user=await AgoraUser.find(map)
+    console.log(user)
+    }
+    else{
+      console.log("...hello from type 6,7")
+      user=await AgoraUser.find({email})
+      console.log(user)
+    }
+
+     if(!user.length ){
+      console.log('... user does not exist ...')
+ 
+      if(type==2 ||type==7)
+      {
+      if(type==7){
+        console.log("...creating password ...")
+        const salt=await bcrypt.genSalt(10)
+        console.log(salt)
+        const hashedPassword=await bcrypt.hash(password,salt);
+        console.log(hashedPassword)
+        result['password']=hashedPassword;
+        console.log(result)
+      }
+
+      //create user to get id to use it generating token
+
+      const newUser=await AgoraUser.create(result);
+      transporter.sendMail(mailOptions)
+      
+    
+      
+
+      console.log("new user ..."+newUser)
+      //insert data to result 
+      result['token']=await agoraSignAccessToken(newUser._id,token_expire_date);
+      result['created_at']=new Date(2023, 5, 28, 20, 0, 0).getTime();
+      result['access_token']=await agoraSignAccessToken(newUser._id,access_token_expire_date)
+      result['expire_date']=new Date(new Date(2023, 5, 28, 20, 0, 0).getTime() + 1 * 24 * 60 * 60 * 1000)
+     
+      //update item that we just created by result object
+      const agoraUser=await AgoraUser.findByIdAndUpdate({_id:newUser._id},result,{new:true})
+    
+    
+      
+    res.json({code:0,data:agoraUser,msg:'user has been created'})
+      // console.log("... populated ...",populatedUser);
+  }      
+      else{
+        res.json({code:-1,data:{},msg:'user not exist and can not login with it'});
+      }
+      
+      }
+     else{
+           
+      console.log('search is not empty')
+    
+    if(type==2 || type==6){
+      if(type==6){
+        console.log("...hello from compare")
+        const isMatch=await user[0].isValidPassword(result.password);
+
+      // const isMatch = await bcrypt.compare(result.password, user.password); 
+       console.log(isMatch)
+       if(!isMatch){
+        throw createError.NotFound("incorrect password")
+        }
+      }
+      const access_token=await agoraSignAccessToken(user[0]._id,access_token_expire_date);
+      const expire_date=new Date(new Date(2023, 5, 28, 20, 0, 0).getTime() + 1 * 24 * 60 * 60 * 1000);
+      const agoraUser=await AgoraUser.findByIdAndUpdate({_id:user[0]._id},{access_token:access_token,expire_date:expire_date},{new:true});
+      
+      res.json({code:0,data:agoraUser,msg:'user information updated'});}
+     
+     else{
+      res.json({code:-1,data:{},msg:'user exist and you can not signup with it'});
+     }}
+  } catch (error) {
+    
+   res.json({code:-1,data:{},msg:'error'})
+  }
+})
+router.post('/reset-password',async(req,res,next)=>{
+  const {email}=req.body;
+  const mailOptions={
+    from:process.env.AUTH_EMAIL,
+    to: email,
+    subject:"reset password",
+    html:`<p>$  </p>`
+};
+
+  const user =await AgoraUser.find({email})
+  console.log(user)
+  if(user[0]){
+    const token=await agoraSignAccessToken(user[0]._id,'15m');
+
+    res.json({code:0,data:{token},msg:'reset password token'})
+  }
+  else{
+  res.json({code:-1,data:{},msg:'user does not exist'})}
+})
+
+router.post('/confirm-reset-password',async(rq,res,next)=>{
+  const {email ,passwordToken}= req.body;
+
+})
+
+
+router.post('/refresh-token',async(req,res,next)=>{
+    res.send("refresh token route")
+})
+router.delete('/logout',async(req,res,next)=>{
+    res.send("logout route")
+})
+router.post('/bind_fcmtoken',async(req,res,next)=>{
+  const {token,fcmtoken}=req.body;
+  if(!fcmtoken){
+    res.json({'code':-1,data:{},msg:"error getting the token"})
+  }
+  const User=await AgoraUser.findByIdAndUpdate({token},{fcmtoken},{new:true})
+  res.json({'code':0,data:{},msg:"success"})
+})
+
+
+module.exports=router;
